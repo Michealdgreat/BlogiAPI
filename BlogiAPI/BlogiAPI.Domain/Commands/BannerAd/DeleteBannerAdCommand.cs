74aa6c1f2b03d3b1a443bad98cd1938d1d68b2fd
michealdgreat <michealshodamola01@gmail.com>
2025-04-09 14:50:05 +0300

    Update DeleteBanner command Inheritance

--- BlogiAPI/BlogiAPI.Domain/Commands/BannerAd/DeleteBannerAdCommand.cs
+++ BlogiAPI/BlogiAPI.Domain/Commands/BannerAd/DeleteBannerAdCommand.cs
@@ -1,8 +1,9 @@
+using BlogiAPI.Domain.Commands.Base;
 using BlogiAPI.Domain.User;
 
 namespace BlogiAPI.Domain.Commands.BannerAd;
 
-public class DeleteBannerAdCommand
+public class DeleteBannerAdCommand : ICommandBase
 {
     public Guid BannerId { get; set; }
     public UserInfo? CommandSender { get; set; }
