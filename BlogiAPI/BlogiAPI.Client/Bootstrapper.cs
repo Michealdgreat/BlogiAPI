7bccb8d6c10ae0a95a907d8ddd83fc6ebfcb3a0c
michealdgreat <michealshodamola01@gmail.com>
2025-04-09 15:35:31 +0300

    Register Client Dependencies.

--- BlogiAPI/BlogiAPI.Client/Bootstrapper.cs
+++ BlogiAPI/BlogiAPI.Client/Bootstrapper.cs
@@ -1,15 +1,19 @@
 using Microsoft.Extensions.DependencyInjection;
 using BlogiAPI.Client.Orchestrators;
-using BlogiAPI.Domain.Repositories;
 using BlogiAPI.Domain.Repositories.Base;
 using BlogiAPI.Domain.Services.Auth;
-
+using BlogiAPI.Domain.Services.BannerAdService.CommandService;
+using BlogiAPI.Domain.Services.BannerAdService.QueryServices;
 using BlogiAPI.Domain.Services.RoleService.CommandService;
 using BlogiAPI.Domain.Services.RoleService.QueryServices;
-
 using BlogiAPI.Domain.Services.UserServices.CommandServices;
 using BlogiAPI.Domain.Services.UserServices.QueryServices;
-
+using BlogiAPI.Domain.Services.CarouselBannerService.CommandService;
+using BlogiAPI.Domain.Services.CarouselBannerService.QueryServices;
+using BlogiAPI.Domain.Services.CategoryService.CommandService;
+using BlogiAPI.Domain.Services.CategoryService.QueryServices;
+using BlogiAPI.Domain.Services.PostService.CommandService;
+using BlogiAPI.Domain.Services.PostService.QueryServices;
 
 namespace BlogiAPI.Client
 {
@@ -18,29 +22,34 @@ namespace BlogiAPI.Client
         public static void RegisterOrchestrators(this IServiceCollection services)
         {
             // Register command services
-           
             services.AddScoped<RoleCommandService>();
-           
             services.AddScoped<UserCommandServices>();
-            
-            services.AddSingleton<AuthService>();
-
+            services.AddScoped<CategoryCommandService>();
+            services.AddScoped<PostCommandService>();
+            services.AddScoped<BannerAdCommandService>();
+            services.AddScoped<CarouselBannerCommandService>();
 
             // Register query services
             services.AddScoped<RoleQueryService>();
             services.AddScoped<UserQueryService>();
-           
+            services.AddScoped<CategoryQueryService>();
+            services.AddScoped<PostQueryService>();
+            services.AddScoped<BannerAdQueryService>();
+            services.AddScoped<CarouselBannerQueryService>();
+
+            // Register AuthService
+            services.AddSingleton<AuthService>();
 
             // Register orchestrators
             services.AddScoped<RoleOrchestrator>();
             services.AddScoped<UserOrchestrator>();
-            
-            
-         services.AddSingleton<IBaseRepository, BaseRepository>();
-           
-            
-            
-           
+            services.AddScoped<CategoryOrchestrator>();
+            services.AddScoped<PostOrchestrator>();
+            services.AddScoped<BannerAdOrchestrator>();
+            services.AddScoped<CarouselBannerOrchestrator>();
+
+            // Register base repository
+            services.AddSingleton<IBaseRepository, BaseRepository>();
         }
     }
 }
